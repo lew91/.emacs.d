96e84e93a25cd1255c9c6ca1fa903032fd3a15df
lew91 <jakelewnet@gmail.com>
2019-05-14 17:14:30 +0800

    markdown-mode config

--- config/init-markdown.el
+++ config/init-markdown.el
@@ -1,6 +1,39 @@
+;;; init-markdown.el --- Insert description here -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
 (require 'markdown-mode)
 
+(defun markdown-imenu-index ()
+  (let* ((patterns '((nil "^#\\([# ]*[^#\n\r]+\\)" 1))))
+    (save-excursion
+      (imenu--generic-function patterns))))
+
+(defun markdown-mode-hook-setup ()
+  ;; Stolen from http://stackoverflow.com/a/26297700
+  ;; makes markdown tables saner via orgtbl-mode
+  ;; Insert org table and it will be automatically converted
+  ;; to markdown table
+  (unless (featurep 'org-table) (require 'org-table))
+  (defun cleanup-org-tables ()
+    (save-excursion
+      (goto-char (point-min))
+      (while (search-forward "-+-" nil t) (replace-match "-|-"))))
+  (add-hook 'after-save-hook 'cleanup-org-tables nil 'make-it-local)
+  (orgtbl-mode 1) ; enable key bindings
+  ;; don't wrap lines because there is table in `markdown-mode'
+  (setq truncate-lines t)
+  (setq imenu-create-index-function 'markdown-imenu-index))
+(add-hook 'markdown-mode-hook 'markdown-mode-hook-setup)
+
+(after-load 'markdown-mode
+  '(progn
+     ;; `pandoc' is better than obsolete `markdown'
+     ;; Temporarily not installed，Maybe need an absolute path to verify the pandoc if exited.
+     (when (executable-find "pandoc")
+       (setq markdown-command "pandoc -f markdown"))))
 
 
 
 (provide 'init-markdown)
+;;; init-markdown.el ends here
