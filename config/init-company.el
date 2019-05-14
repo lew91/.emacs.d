16845eeb4c40cef27846f991a1044c9cd77dd50b
lew91 <jakelewnet@gmail.com>
2019-05-14 15:38:02 +0800

    解决依赖require-pakcage

--- config/init-company.el
+++ config/init-company.el
@@ -11,21 +11,23 @@
 (setq tab-always-indent 'complete)
 (add-to-list 'completion-styles 'initials t)
 
-(when (maybe-require-package 'company)
-  (add-hook 'after-init-hook 'global-company-mode)
-  (after-load 'company
-    (dolist (backend '(company-eclim company-semantic))
-      (delq backend company-backends))
-    (diminish 'company-mode)
-    ;;(define-key company-mode-map (kbd "M-/") 'company-complete)
-    ;;(define-key company-active-map (kbd "M-/") 'company-other-backend)
-    (define-key company-active-map (kbd "C-n") 'company-select-next)
-    (define-key company-active-map (kbd "C-p") 'company-select-previous)
-    (setq-default company-dabbrev-other-buffers 'all
-                  company-tooltip-align-annotations t))
-  (global-set-key (kbd "M-C-/") 'company-complete)
-  (when (maybe-require-package 'company-quickhelp)
-    (add-hook 'after-init-hook 'company-quickhelp-mode)))
+
+
+(add-hook 'after-init-hook 'global-company-mode)
+
+(after-load 'company
+  (dolist (backend '(company-eclim company-semantic))
+    (delq backend company-backends))
+  (diminish 'company-mode)
+  ;;(define-key company-mode-map (kbd "M-/") 'company-complete)
+  ;;(define-key company-active-map (kbd "M-/") 'company-other-backend)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (setq-default company-dabbrev-other-buffers 'all
+                company-tooltip-align-annotations t))
+(global-set-key (kbd "M-C-/") 'company-complete)
+
+(add-hook 'company-mode-hook 'company-quickhelp-mode)
 
 ;; Suspend page-break-lines-mode while company menu is active
 ;; (see https://github.com/company-mode/company-mode/issues/416)
