cb8494d94fcb1ca8eae66f234f0bc18094a2a2dc
lew91 <jakelewnet@gmail.com>
2019-05-19 22:26:21 +0800

    fix ycmd

--- config/init-ycmd.el
+++ config/init-ycmd.el
@@ -2,19 +2,30 @@
 
 (set-variable 'ycmd-server-command '("python3" "/users/jakelew/.vim/bundle/YouCompleteMe/third_party/ycmd/ycmd/"))
 (set-variable 'ycmd-global-config (expand-file-name "~/.vim/bundle/YouCompleteMe/third_party/ycmd/.ycm_extra_conf.py"))
-(add-hook 'C++-mode-hook 'ycmd-mode)
+;;(add-hook 'C++-mode-hook 'ycmd-mode)
 ;;(add-hook 'after-init-hook 'global-ycmd-mode)
 
+(dolist (hook (list
+               'c-mode-hook
+               'c++-mode-hook
+               )
+              )
+  (add-hook hook '(lambda()
+                    (ycmd-mode)
+                    )))
 
-(after-load 'ycmd-mode
-  (require 'company-ycmd)
-  (company-ycmd-setup)
-  (add-to-list 'company-backends (company-mode/backend-with-yas 'company-ycmd)))
 
-(after-load 'ycmd-mode
-  (require 'flycheck-ycmd)
-  (flycheck-ycmd-setup)
-  (add-hook 'ycmd-mode-hook 'flycheck-ycmd-setup)
-  (add-hook 'ycmd-mode-hook 'ycmd-eldoc-setup))
+
+
+(require 'company-ycmd)
+(company-ycmd-setup)
+(add-to-list 'company-backends (company-mode/backend-with-yas 'company-ycmd))
+
+(require 'flycheck-ycmd)
+(flycheck-ycmd-setup)
+(add-hook 'ycmd-mode-hook 'flycheck-ycmd-setup)
+
+(require 'ycmd-eldoc)
+(add-hook 'ycmd-mode-hook 'ycmd-eldoc-setup)
 
 (provide 'init-ycmd)
