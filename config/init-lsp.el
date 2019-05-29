7d7ad23d9ca574bd559d1bad4c8e786d0dc07820
lew91 <jakelewnet@gmail.com>
2019-05-29 20:39:43 +0800

    add advice projectile root dir

--- config/init-lsp.el
+++ config/init-lsp.el
@@ -11,4 +11,26 @@
                     (lsp)
                     )))
 
+(after-load 'lsp
+  (setq lsp-prefer-flymake nil)
+  (maybe-require-package 'lsp-ui)
+  (setq lsp-ui-doc-include-signature t)
+  (setq lsp-ui-doc-header t))
+
+
+(defun /lsp/suggest-project-root ()
+  "suggest the nearest project that is not a dependency."
+  (or
+   (locate-dominating-file
+    (buffer-file-name)
+    (lambda (dir)
+      (if (string-match-p "node_modules" dir)
+          nil
+        (file-exists-p (concat dir "package.json")))))
+   (projectile-project-root)))
+
+(after 'lsp-mode
+       (advice-add #'lsp--suggest-project-root :override #'/lsp/suggest-project-root))
+
+
 (provide 'init-lsp)
