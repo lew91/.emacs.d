add794b108971b0635a9d903a594c0ec4f70d255
lew91 <jakelewnet@gmail.com>
2019-06-04 11:30:53 +0800

    rebuild editing util & eamcs-lisp config

---
+++ config/init-editing.el
@@ -0,0 +1,239 @@
+(require 'avy)
+(require 'anzu)
+(require 'browse-kill-ring)
+(require 'expand-region)
+(require 'unfill)
+(require 'diff-hl)
+(require 'hungry-delete)
+(require 'highlight-escape-sequences)
+(require 'image-file)
+(require 'vlf)
+(require 'goto-line-preview)
+(require 'symbol-overlay)
+(require 'multiple-cursors)
+(require 'expand-region)
+(require 'rainbow-delimiters)
+(require 'move-dup)
+(require 'whole-line-or-region)
+(require 'diminish)
+
+(electric-pair-mode 1)
+(global-anzu-mode)
+(auto-image-file-mode 1)
+(cua-selection-mode t)                  ; for rectangles, CUA is nice
+
+(setq avy-background t)
+(setq avy-style 'at-full)
+(setq-default hungry-delete-chars-to-skip " \t\f\v")
+(setq browse-kill-ring-separator "\f")
+(setq show-paren-style 'mixed)
+
+;; Don't disable narrowing commands
+(put 'narrow-to-region 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
+(put 'narrow-to-defun 'disabled nil)
+
+;; Don't disable case-change  functions
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
+
+
+
+(add-hook 'prog-mode-hook 'diff-hl-mode)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'after-init-hook 'global-auto-revert-mode)
+(setq global-auto-revert-non-file-buffers t
+      auto-revert-verbose nil)
+(add-hook 'after-init-hook 'transient-mark-mode)
+(add-hook 'after-init-hook 'global-hungry-delete-mode)
+(add-hook 'after-init-hook 'show-paren-mode)
+(add-hook 'after-init-hook 'hes-mode)
+
+(when (fboundp 'display-line-numbers-mode)
+  (setq-default display-line-numbers-width 3)
+  (add-hook 'prog-mode-hook 'display-line-numbers-mode))
+
+(when (fboundp 'global-prettify-symbols-mode)
+  (add-hook 'after-init-hook 'global-prettify-symbols-mode))
+
+(add-hook 'after-init-hook 'whole-line-or-region-global-mode)
+
+
+(global-set-key [remap goto-line] 'goto-line-preview)
+(after-load 'page-break-lines
+  (push 'browse-kill-ring-mode page-break-lines-modes))
+
+
+
+(diminish 'anzu-mode)
+(diminish 'hungry-delete-mode)
+(after-load 'autorevert
+  (diminish 'auto-revert-mode))
+(after-load 'subword
+  (diminish 'subword-mode))
+(diminish 'symbol-overlay-mode)
+(after-load 'page-break-lines
+  (diminish 'page-break-lines-mode))
+(after-load 'whole-line-or-region
+  (diminish 'whole-line-or-region-local-mode))
+
+
+
+(dolist (hook (list
+               'prog-mode-hook
+               'html-mode-hook
+               'yaml-mode-hook
+               'conf-mode-hook))
+  (add-hook hook '(lambda ()
+                    (symbol-overlay-mode))))
+
+
+
+(defun ffap-vlf ()
+  "Find file at point with VLF."
+  (interactive)
+  (let ((file (ffap-file-at-point)))
+    (unless (file-exists-p file)
+      (error "File does not exist: %s" file))
+    (vlf file)))
+
+(global-set-key (kbd "RET") 'newline-and-indent)
+(defun lew/newline-at-end-of-line ()
+  "Move to end of line, enter a newline, and reindent."
+  (interactive)
+  (move-end-of-line 1)
+  (newline-and-indent))
+
+(when (fboundp 'display-line-numbers-mode)
+    (defun lew/with-display-line-numbers (f &rest args)
+      (let ((display-line-numbers t))
+        (apply f args)))
+    (advice-add 'goto-line-preview :around #'lew/with-display-line-numbers))
+
+(defun kill-back-to-indentation ()
+  "Kill from point back to the first non-whitespace character on the line."
+  (interactive)
+  (let ((prev-pos (point)))
+    (back-to-indentation)
+    (kill-region (point) prev-pos)))
+
+(global-set-key (kbd "C-M-<backspace>") 'kill-back-to-indentation)
+
+(defun lew/backward-up-sexp (arg)
+  "Jump up to the start of the ARG'th enclosing sexp."
+  (interactive "p")
+  (let ((ppss (syntax-ppss)))
+    (cond ((elt ppss 3)
+           (goto-char (elt ppss 8))
+           (sanityinc/backward-up-sexp (1- arg)))
+          ((backward-up-list arg)))))
+
+(global-set-key [remap backward-up-list] 'lew/backward-up-sexp) ; C-M-u, C-M-up
+
+(defun lew/open-line-with-reindent (n)
+  "A version of `open-line' which reindents the start and end positions.
+If there is a fill prefix and/or a `left-margin', insert them
+on the new line if the line would have been blank.
+With arg N, insert N newlines."
+  (interactive "*p")
+  (let* ((do-fill-prefix (and fill-prefix (bolp)))
+         (do-left-margin (and (bolp) (> (current-left-margin) 0)))
+         (loc (point-marker))
+         ;; Don't expand an abbrev before point.
+         (abbrev-mode nil))
+    (delete-horizontal-space t)
+    (newline n)
+    (indent-according-to-mode)
+    (when (eolp)
+      (delete-horizontal-space t))
+    (goto-char loc)
+    (while (> n 0)
+      (cond ((bolp)
+             (if do-left-margin (indent-to (current-left-margin)))
+             (if do-fill-prefix (insert-and-inherit fill-prefix))))
+      (forward-line 1)
+      (setq n (1- n)))
+    (goto-char loc)
+    (end-of-line)
+    (indent-according-to-mode)))
+
+(global-set-key (kbd "C-o") 'lew/open-line-with-reindent)
+
+
+(defun lew/disable-features-during-macro-call (orig &rest args)
+  "When running a macro, disable features that might be expensive.
+ORIG is the advised function, which is called with its ARGS."
+  (let (post-command-hook
+        font-lock-mode
+        (tab-always-indent (or (eq 'complete tab-always-indent) tab-always-indent)))
+    (apply orig args)))
+
+(advice-add 'kmacro-call-macro :around 'lew/disable-features-during-macro-call)
+
+;; Hack for setting a fixed wrap column in visual-line-mode.
+(defun lew/set-visual-wrap-column (new-wrap-column &optional buffer)
+  "Force visual line wrap at NEW-WRAP-COLUMN in BUFFER (defaults
+    to current buffer) by setting the right-hand margin on every
+    window that displays BUFFER.  A value of NIL or 0 for
+    NEW-WRAP-COLUMN disables this behavior."
+  (interactive (list (read-number "New visual wrap column, 0 to disable: " (or visual-wrap-column fill-column 0))))
+  (if (and (numberp new-wrap-column)
+           (zerop new-wrap-column))
+      (setq new-wrap-column nil))
+  (with-current-buffer (or buffer (current-buffer))
+    (visual-line-mode t)
+    (set (make-local-variable 'visual-wrap-column) new-wrap-column)
+    (add-hook 'window-configuration-change-hook 'update-visual-wrap-column nil t)
+    (let ((windows (get-buffer-window-list)))
+      (while windows
+        (when (window-live-p (car windows))
+          (with-selected-window (car windows)
+            (update-visual-wrap-column)))
+        (setq windows (cdr windows))))))
+
+(defun lew/update-visual-wrap-column ()
+  (if (not visual-wrap-column)
+      (set-window-margins nil nil)
+    (let* ((current-margins (window-margins))
+           (right-margin (or (cdr current-margins) 0))
+           (current-width (window-width))
+           (current-available (+ current-width right-margin)))
+      (if (<= current-available visual-wrap-column)
+          (set-window-margins nil (car current-margins))
+        (set-window-margins nil (car current-margins)
+                            (- current-available visual-wrap-column))))))
+
+;; A function for easily editing a file as root through TRAMP.
+(defun sudo-edit (&optional arg)
+  (interactive "p")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (if (fboundp 'helm-read-file-name)
+                             (helm-read-file-name "File: ")
+                           (ido-read-file-name "File: "))))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+
+(defun lew/css-expand-statement ()
+  (interactive)
+  (save-excursion
+    (end-of-line)
+    (search-backward "{")
+    (forward-char 1)
+    (let ((beg (point)))
+      (newline)
+      (er/mark-inside-pairs)
+      (replace-regexp ";" ";\n" nil (region-beginning) (region-end))
+      (indent-region beg (point)))))
+
+(defun lew/css-contract-statement ()
+  (interactive)
+  (end-of-line)
+  (search-backward "{")
+  (while (not (looking-at "}"))
+    (join-line -1))
+  (back-to-indentation))
+
+
+(provide 'init-editing)
---
+++ config/init-elisp.el
@@ -0,0 +1,153 @@
+;;; init-elisp.el  --- Additional functionality for elisp, eldoc
+
+(require 'pp)
+(require 'elisp-slime-nav)
+(require 'auto-compile)
+(require 'cl-lib-highlight)
+(require 'highlight-quoted)
+
+(dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
+  (add-hook hook 'turn-on-elisp-slime-nav-mode))
+(add-hook 'emacs-lisp-mode-hook (lambda () (setq mode-name "ELisp")))
+
+;; Automatic byte compilation
+(add-hook 'after-init-hook 'auto-compile-on-save-mode)
+(add-hook 'after-init-hook 'auto-compile-on-load-mode)
+
+(setq load-prefer-newer t) ; load .el if newer than corresponding .elc
+
+(after-load 'lisp-mode
+  (cl-lib-highlight-initialize))
+
+(add-hook 'emacs-lisp-mode-hook 'highlight-quoted-mode)
+
+(defun lew/insert-headerise-elisp ()
+  "Add minimal header and footer to an elisp buffer in order to placate flycheck."
+  (interactive)
+  (let ((fname (if (buffer-file-name)
+                   (file-name-nondirectory (buffer-file-name))
+                 (error "This buffer is not visiting a file"))))
+    (save-excursion
+      (goto-char (point-min))
+      (insert ";;; " fname " --- Insert description here -*- lexical-binding: t -*-\n"
+              ";;; Commentary:\n"
+              ";;; Code:\n\n")
+      (goto-char (point-max))
+      (insert ";;; " fname " ends here\n"))))
+
+(defun lew/eval-last-sexp-or-region (prefix)
+  "Eval region from BEG to END if active, otherwise the last sexp."
+  (interactive "P")
+  (if (and (mark) (use-region-p))
+      (eval-region (min (point) (mark)) (max (point) (mark)))
+    (pp-eval-last-sexp prefix)))
+
+(global-set-key [remap eval-expression] 'pp-eval-expression)
+
+(after-load 'lisp-mode
+  (define-key emacs-lisp-mode-map (kbd "C-x C-e") 'lew/eval-last-sexp-or-region))
+
+(defun lew/make-read-only (expression out-buffer-name)
+  "Enable `view-mode' in the output buffer - if any - so it can be closed with `\"q\"."
+  (when (get-buffer out-buffer-name)
+    (with-current-buffer out-buffer-name
+      (view-mode 1))))
+(advice-add 'pp-display-expression :after 'lew/make-read-only)
+
+
+(defun lew/maybe-set-bundled-elisp-readonly ()
+  "If this elisp appears to be part of Emacs, then disallow editing."
+  (when (and (buffer-file-name)
+             (string-match-p "\\.el\\.gz\\'" (buffer-file-name)))
+    (setq buffer-read-only t)
+    (view-mode t)))
+
+(add-hook 'emacs-lisp-mode-hook 'lew/maybe-set-bundled-elisp-readonly)
+
+(defun lew/eval-buffer (arg)
+  "Execute the current buffer as Lisp code.
+Top-level forms are evaluated with `eval-defun' so that `defvar'
+and `defcustom' forms reset their default values."
+  (interactive "P")
+  (if (equal arg (list 4))
+      (save-excursion
+        (goto-char (point-min))
+        (while (not (eobp))
+          (forward-sexp)
+          (eval-defun nil))
+        (message "Redefined buffer!"))
+    (eval-buffer)))
+
+;; Support byte-compilation in a sub-process, as required by highlight-cl
+(defun lew/byte-compile-file-batch (filename)
+  "Byte-compile FILENAME in batch mode, ie. a clean sub-process."
+  (interactive "fFile to byte-compile in batch mode: ")
+  (let ((emacs (car command-line-args)))
+    (compile
+     (concat
+      emacs " "
+      (mapconcat
+       'shell-quote-argument
+       (list "-Q" "-batch" "-f" "batch-byte-compile" filename)
+       " ")))))
+
+(defun lew/auto-compile-load-after-compile (success)
+  "Reload the current emacs-lisp file after it's recompiled, if an older version is loaded."
+  (when (eq success t)
+    (let ((buffer-path (file-truename buffer-file-name)))
+      (when (assoc buffer-path load-history)
+        (load-file buffer-path)))))
+
+(advice-add 'auto-compile-byte-compile :filter-return #'lew/auto-compile-load-after-compile)
+
+(defun lew/maybe-remove-elc (&rest _)
+  "If reverting from VC, delete any .elc file that will now be out of sync."
+  (when sanityinc/vc-reverting
+    (when (and (eq 'emacs-lisp-mode major-mode)
+               buffer-file-name
+               (string= "el" (file-name-extension buffer-file-name)))
+      (let ((elc (concat buffer-file-name "c")))
+        (when (file-exists-p elc)
+          (message "Removing out-of-sync elc file %s" (file-name-nondirectory elc))
+          (delete-file elc))))))
+
+;;;###autoload
+(defun lew/pp-eval-expression (expression)
+  "Same as 'pp-eval-expression' but without \"Evaluating..\" message."
+  (interactive
+   (list (read--expression "Eval: ")))
+  (setq values (cons (eval expression) values))
+  (pp-display-expression (car values) "*Pp Eval Output*"))
+
+
+
+;;;###autoload
+(defun lew/eval-dwin (arg)
+  "Eval last sexp or region if it is active. ARG is passed to 'eval-last-sexp'."
+  (interactive "P")
+  (if (use-region-p)
+      (eval-region (region-beginning) (region-end))
+    (eval-last-sexp arg)))
+
+;;;###autoload
+(defun lew/pp-veal-dwin (arg)
+  "Eval last sexp or region if it is active. ARG is passed to 'pp-eval-last-sexp'."
+  (interactive "P")
+  (if (use-region-p)
+      (eval-region (region-beginning) (region-end))
+    (pp-eval-last-sexp arg)))
+
+;;;###autoload
+(defun lew/indent-sexp (&optional no-offset pp)
+  "Indent each line of the list starting just after point.
+If NO-OFFSET is non-nil (with \\[universal-argument]), indent
+without offset for the following lines.
+If PP is non-nil (with \\[universal-argument] \\[universal-argument]), pretty-print the following list."
+  (interactive
+   (list (equal current-prefix-arg '(4))
+         (equal current-prefix-arg '(16))))
+  (let ((lisp-indent-offset (and no-offset 1)))
+    (indent-pp-sexp pp)))
+
+(provide 'init-elisp)
+;;; init-elisp.el ends here
