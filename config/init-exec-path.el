bb7f31e7997a50ae9b23ad8bf96437ee341ae76d
lew91 <jakelewnet@gmail.com>
2019-06-15 06:31:02 +0800

    disable the exec-path-from-shell-check warning message

--- config/init-exec-path.el
+++ config/init-exec-path.el
@@ -4,6 +4,7 @@
   (dolist (var '("PATH" "SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO"))
     (add-to-list 'exec-path-from-shell-variables var)))
 
+(setq exec-path-from-shell-check-startup-files nil)
 
 (when (or (memq window-system '(mac ns x))
           (unless (memq system-type '(ms-dos windows-nt))
