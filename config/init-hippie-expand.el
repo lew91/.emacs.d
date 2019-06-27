62ff8c7effcc66c88e7848b1a3e0539510a3f1fe
lew91 <jakelewnet@gmail.com>
2019-06-27 08:36:16 +0800

    hippie-expand enhance

--- config/init-hippie-expand.el
+++ config/init-hippie-expand.el
@@ -1,3 +1,8 @@
+;;; init-hippie-expand.el --- Hippie Expand enhance -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(require 'hippie-exp)
 
 (global-set-key (kbd "M-/") 'hippie-expand)
 
@@ -8,4 +13,170 @@
         try-expand-dabbrev-all-buffers
         try-expand-dabbrev-from-kill))
 
+;; ref from https://www.emacswiki.org/emacs/HippieExpand
+;; Create a hippie-expand function with a given list of strings
+(defvar dcsh-command-list nil)
+
+(setq dcsh-command-list '("all_registers"
+                          "check_design" "check_test" "compile" "current_design"
+                          "link" "uniquify"
+                          "report_timing" "report_clocks" "report_constraint"
+                          "get_unix_variable" "set_unix_variable"
+                          "set_max_fanout"
+                          "report_area" "all_clocks" "all_inputs" "all_outputs"))
+
+(defun he-dcsh-command-beg ()
+  (let ((p))
+    (save-excursion
+      (backward-word 1)
+      (setq p (point)))
+    p))
+
+(defun try-expand-dcsh-command (old)
+  (unless old
+    (he-init-string (he-dcsh-command-beg) (point))
+    (setq he-expand-list (sort
+                          (all-completions he-search-string (mapcar 'list dcsh-command-list))
+                          'string-lessp)))
+  (while (and he-expand-list
+              (he-string-member (car he-expand-list) he-tried-table))
+    (setq he-expand-list (cdr he-expand-list)))
+  (if (null he-expand-list)
+      (progn
+        (when old (he-reset-string))
+        ())
+    (he-substitute-string (car he-expand-list))
+    (setq he-tried-table (cons (car he-expand-list) (cdr he-tried-table)))
+    (setq he-expand-list (cdr he-expand-list))
+    t))
+
+;; Substring expansion
+;; from https://www.emacswiki.org/emacs/HippieExpand
+(defun try-jl-dabbrev-substring (old)
+  (let ((old-fun (symbol-function 'he-dabbrev-search)))
+    (fset 'he-dabbrev-search (symbol-function 'jl-dabbrev-substring-search))
+    (unwind-protect
+        (try-expand-dabbrev old)
+      (fset 'he-dabbrev-search old-fun))))
+
+
+(defun jl-dabbrev-substring-search (pattern &optional reverse limit)
+  (let ((result ())
+	(regpat (cond ((not hippie-expand-dabbrev-as-symbol)
+		       (concat (regexp-quote pattern) "\\sw+"))
+		      ((eq (char-syntax (aref pattern 0)) ?_)
+		       (concat (regexp-quote pattern) "\\(\\sw\\|\\s_\\)+"))
+		      (t
+		       (concat (regexp-quote pattern)
+			       "\\(\\sw\\|\\s_\\)+")))))
+    (while (and (not result)
+		(if reverse
+		    (re-search-backward regpat limit t)
+		  (re-search-forward regpat limit t)))
+      (setq result (buffer-substring-no-properties (save-excursion
+                                                     (goto-char (match-beginning 0))
+                                                     (skip-syntax-backward "w_")
+                                                     (point))
+						   (match-end 0)))
+      (if (he-string-member result he-tried-table t)
+	  (setq result nil)))     ; ignore if bad prefix or already in table
+    result))
+
+;; Flexible match expansion
+;; from https://www.emacswiki.org/emacs/HippieExpand
+(defun try-expand-flexible-abbrev (old)
+  "Try to complete word using flexible matching.
+
+Flexible matching works by taking the search string and then
+interspersing it with a regexp for any character. So, if you try
+to do a flexible match for `foo' it will match the word
+`findOtherOtter' but also `fixTheBoringOrange' and
+`ifthisisboringstopreadingnow'.
+
+The argument OLD has to be nil the first call of this function, and t
+for subsequent calls (for further possible completions of the same
+string).  It returns t if a new completion is found, nil otherwise."
+  (if (not old)
+      (progn
+	(he-init-string (he-lisp-symbol-beg) (point))
+	(if (not (he-string-member he-search-string he-tried-table))
+	    (setq he-tried-table (cons he-search-string he-tried-table)))
+	(setq he-expand-list
+	      (and (not (equal he-search-string ""))
+		   (he-flexible-abbrev-collect he-search-string)))))
+  (while (and he-expand-list
+	      (he-string-member (car he-expand-list) he-tried-table))
+    (setq he-expand-list (cdr he-expand-list)))
+  (if (null he-expand-list)
+      (progn
+	(if old (he-reset-string))
+	())
+      (progn
+	(he-substitute-string (car he-expand-list))
+	(setq he-expand-list (cdr he-expand-list))
+	t)))
+
+(defun he-flexible-abbrev-collect (str)
+  "Find and collect all words that flex-matches STR.
+See docstring for `try-expand-flexible-abbrev' for information
+about what flexible matching means in this context."
+  (let ((collection nil)
+        (regexp (he-flexible-abbrev-create-regexp str)))
+    (save-excursion
+      (goto-char (point-min))
+      (while (search-forward-regexp regexp nil t)
+        ;; Is there a better or quicker way than using
+        ;; `thing-at-point' here?
+        (setq collection (cons (thing-at-point 'word) collection))))
+    collection))
+
+(defun he-flexible-abbrev-create-regexp (str)
+  "Generate regexp for flexible matching of STR.
+See docstring for `try-expand-flexible-abbrev' for information
+about what flexible matching means in this context."
+  (concat "\\b" (mapconcat (lambda (x) (concat "\\w*" (list x))) str "")
+          "\\w*" "\\b"))
+
+(setq hippie-expand-try-functions-list
+      (cons 'try-expand-flexible-abbrev hippie-expand-try-functions-list))
+
+
+;; Expansion menu
+;; from https://www.emacswiki.org/emacs/HippieExpand
+(defun jl-hippie-expand-completions (&optional hippie-expand-function)
+  "Return the full list of possible completions generated by `hippie-expand'. The optional argument can be generated with `make-hippie-expand-function'."
+  (let ((this-command 'jl-hippie-expand-completions)
+        (last-command last-command)
+        (buffer-modified (buffer-modified-p))
+        (hippie-expand-function (or hippie-expand-function 'hippie-expand)))
+    (cl-flet ((ding)) ; avoid the (ding) when hippie-expand exhausts its options.
+      (while (progn
+               (funcall hippie-expand-function nil)
+               (setq last-command 'jl-hippie-expand-completions)
+               (not (equal he-num -1)))))
+    ;; Evaluating the completions modifies the buffer, however we will finish
+    ;; up in the same state that we began.
+    (set-buffer-modified-p buffer-modified)
+    ;; Provide the options in the order in which they are normally generated.
+    (delete he-search-string (reverse he-tried-table))))
+
+(defun jl-ido-hippie-expand-with (hippie-expand-function)
+  "Offer ido-based completion using the specified 'hippie-expand function'."
+  (let* ((options (jl-hippie-expand-completions hippie-expand-function))
+         (selection (and options
+                         (ido-completing-read "Completions: " options))))
+    (if selection
+        (he-substitute-string selection t)
+      (message "No expansion found"))))
+
+(defun jl-ido-hippie-expand ()
+  "Offer ido-based completion for the word at point."
+  (interactive)
+  (jl-ido-hippie-expand-with 'hippie-expand))
+
+(global-set-key (kbd "C-c /") 'jl-ido-hippie-expand)
+
+
+
 (provide 'init-hippie-expand)
+;;; init-hippie-expand.el ends here
