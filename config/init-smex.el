a368a056bfab88740340a6b0e1cf29cc8943802c
lew91 <jakelewnet@gmail.com>
2019-06-27 09:32:30 +0800

    make smex more compatible

--- config/init-smex.el
+++ config/init-smex.el
@@ -7,17 +7,13 @@
 
 ;;; Code:
 
-(smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-;; (lazy-load-set-keys
-;;  '(
-;;    ("M-x" . smex)
-;;    ("M-X" . smex-major-mode-commands)
-;;    ("C-c C-c M-x" . execute-extended-command)
-;;    )
-;;  )
+;; (smex-initialize)
+;; (global-set-key (kbd "M-x") 'smex)
+;; (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(global-set-key [remap execute-extended-command] 'smex)
+
 
 
 (provide 'init-smex)
