f2607cf1f6e7687841d891941501cb20998ba771
lew91 <jakelewnet@gmail.com>
2019-07-05 19:43:21 +0800

    diminish flyspell-mode

--- config/init-ispell.el
+++ config/init-ispell.el
@@ -10,7 +10,8 @@
   (define-key flyspell-mode-map (kbd "C-;") nil)  ; 按键与avy-jump 冲突，取消之
   (define-key flyspell-mode-map (kbd "C-.") nil)  ; 与全局set-mark 冲突，取消之
   (setq-default ispell-program-name "aspell")
-  (setq-default ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together")))
+  (setq-default ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together"))
+  (diminish 'flyspell-mode))
 
 
 
