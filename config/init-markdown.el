0feb21cf5fc17450ec3632125117cd8b7e2be117
lew91 <jakelewnet@gmail.com>
2019-07-21 08:22:03 +0800

    markdown to org files function

--- config/init-markdown.el
+++ config/init-markdown.el
@@ -32,6 +32,14 @@
      (when (executable-find "pandoc")
        (setq markdown-command "pandoc -f markdown")))
 
+;; taken from https://emacs.stackexchange.com/questions/5465/how-to-migrate-markdown-files-to-emacs-org-mode-format
+(defun markdown-convert-buffer-to-org ()
+    "Convert the current buffer's content from markdown to orgmode format
+and save it with the current buffer's file name but with .org extension."
+    (interactive)
+    (shell-command-on-region (point-min) (point-max)
+                             (format "pandoc -f markdown -t org -o %s"
+                                     (concat (file-name-sans-extension (buffer-file-name)) ".org"))))
 
 
 (provide 'init-markdown)
