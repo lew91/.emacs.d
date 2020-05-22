a6422fc7afa845217892ad4db3ca1227d54b8b9d
lew91 <jakelewnet@gmail.com>
2020-05-22 23:51:54 +0800

    etags config

---
+++ config/init-etags.el
@@ -0,0 +1,7 @@
+(setq tags-table-list (list (expand-file-name "~/.TAGS/TAGS")))
+(setq tags-default-target-directory "~/.TAGS/")
+(setq tags-default-storage-direction "~/.TAGS/")
+(setq tags-default-suffix "*.el")
+
+
+(provide 'init-etags)
